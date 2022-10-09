70369a87c7b0d2cf902bfad150262211e8c58b07
Kelley Mueller <111308701+KLMueller@users.noreply.github.com>
2022-10-09 16:19:01 -0500

    Update navbar.component.ts
    Cleaned up the currentSearch function and deleted extra imports.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,26 +1,22 @@
-import { getLocaleEraNames } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
 name: string = '';
 search = [];
 
 public currentSearch(){
-  let newSearch = this.name;
-  let currentSearch = this.search.push(newSearch);
-  console.log("test", newSearch, currentSearch)
+ this.search.push(this.name);
+  this.name = '';
+  console.log(this.search)
 }
-
 
 constructor() { }
 
-ngOnInit(): void {
-}
-
+ngOnInit(): void {}
 }
